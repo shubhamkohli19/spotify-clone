058233d0ac96d76229859813005f2967813f3ccd
shubhamkohli19 <shubhamkohli172@gmail.com>
2025-04-03 23:57:51 +0530

    Completed Auth Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import { Button } from "./components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
-function App() {
+export default function App() {
   return (
-    <h1 className="text-6xl font-bold underline">
-    Hello world!
-    <Button>efwi</Button>
-  </h1>
-  )
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 }
-
-export default App;
