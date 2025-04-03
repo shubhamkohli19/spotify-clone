483c486008c318f616a85ca7b2bb48ecc680c594
shubhamkohli19 <shubhamkohli172@gmail.com>
2025-04-03 23:35:48 +0530

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <h1 className="text-6xl font-bold underline">
+    Hello world!
+    <Button>efwi</Button>
+  </h1>
+  )
+}
+
+export default App;
