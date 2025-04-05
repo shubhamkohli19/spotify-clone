cb242cf753939ea266b6bc6ff3f33f629f7e00c9
shubhamkohli19 <shubhamkohli172@gmail.com>
2025-04-05 12:36:14 +0530

    Backend Setup

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import songsRoutes from "./routes/songs.route.js";
+import albumsRoutes from "./routes/albums.route.js";
+import statsRoutes from "./routes/stats.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songsRoutes);
+app.use("/api/albums", albumsRoutes);
+app.use("/api/stats", statsRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
