1c2195834ee2f51a7aed975ce60c508e90a3ef8f
shubhamkohli19 <shubhamkohli699@gmail.com>
2025-04-09 09:44:22 +0530

    Song Route and Controller

--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from './../middleware/auth.middleware';
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Songs route with GET method");
-});
+router.get("/", protectRoute, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", protectRoute, getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
